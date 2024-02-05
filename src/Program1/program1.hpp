a95d04def646238803c51380ab1562eecc8ea982
powerfyng <puunika@mail.ru>
2024-02-05 11:42:18 -0500

    Initial commit

---
+++ src/Program1/program1.hpp
@@ -0,0 +1,44 @@
+#ifndef PROGRAM1_H
+#define PROGRAM1_H
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+class SharedBuffer
+{
+public:
+    void writeToBuffer(const std::string &data);
+    std::string readFromBuffer();
+
+private:
+    std::string _data;
+    std::mutex _mutex;
+    std::condition_variable _cond;
+};
+
+class Thread1
+{
+
+public:
+    Thread1(SharedBuffer &exBuffer) : _buffer(exBuffer) {}
+    void run();
+
+private:
+    SharedBuffer &_buffer;
+};
+
+class Thread2
+{
+
+public:
+    Thread2(SharedBuffer &exBuffer) : _buffer(exBuffer) {}
+    void run();
+
+private:
+    SharedBuffer &_buffer;
+};
+
+#endif
---
+++ src/Program1/program1_main.cpp
@@ -0,0 +1,18 @@
+#include "program1.hpp"
+
+void Thread1::run()
+{
+
+}
+
+void Thread2::run()
+{
+
+}
+
+
+
+int main()
+{
+    return 0;
+}
