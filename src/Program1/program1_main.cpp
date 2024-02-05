89bb8fc743be64eed4c03ed3509870a62e229488
powerfyng <puunika@mail.ru>
2024-02-05 12:03:23 -0500

    (feature) Implemented shared buffer class for the prog1

--- src/Program1/program1_main.cpp
+++ src/Program1/program1_main.cpp
@@ -10,9 +10,36 @@ void Thread2::run()
 
 }
 
+void SharedBuffer::writeToBuffer(const std::string &data)
+{
+    std::unique_lock<std::mutex> lock(_mutex);
+    _data = data;
+    _cond.notify_all();
+}
+
+std::string SharedBuffer::readFromBuffer()
+{
+    std::unique_lock<std::mutex> lock(_mutex);
+    _cond.wait(lock, !_data.empty());
+    std::string tmp = _data;
+    _data.clear();
+    return tmp;
+}
+
+
+
 
 
 int main()
 {
+    SharedBuffer buffer;
+    Thread1 thread1(buffer);
+    Thread2 thread2(buffer);
+
+    std::thread t1(&Thread1::run, &thread1);
+    std::thread t2(&Thread2::run, &thread2);
+
+    t1.join();
+    t2.join();
     return 0;
 }
